c127cd23c5a09bcb4978e87c3c9300b140a841a8
TriIntanSyarifah1011 <intansyarifah2023@gmail.com>
2025-02-24 11:22:23 +0700

    menambahkan file .includes

---
+++ .includes/sidemenu.php
@@ -0,0 +1,78 @@
+<!-- Menu -->
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand demo">
+    <a href="./dashboard.php" class="app-brand-link">
+      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">E-commerce</span>
+    </a>
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+  </div>
+  <div class="menu-inner-shadow"></div>
+  <ul class="menu-inner py-1">
+    <!-- Dashboard -->
+    <li class="menu-item">
+      <a href="dashboard.php" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home-circle"></i>
+        <div data-i18n="Analytics">Dashboard</div>
+      </a>
+    </li>
+    <!-- Forms & Tables -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Menu</span></li>
+    <!-- Forms -->
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-detail"></i>
+        <div data-i18n="Posts">Kategori</div>
+      </a>
+
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="posts.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Post Produk</div>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="posts.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Post Produk</div>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="posts.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Pelanggan</div>
+          </a>
+        </li>
+      </ul>
+
+
+
+      <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-cart-alt"></i>
+        <div data-i18n="Posts">Laman Produk</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="posts.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Produk</div>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="posts.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Pesanan</div>
+          </a>
+        </li>
+      </ul>
+
+</aside>
+<!-- / Menu -->
+ 
